93980fa973420c55303f8c430191f5b8f7391182
jacob c <clxe@users.noreply.github.com>
2018-02-05 20:36:03 -0500

    Dashboard module

--- src/org/usfirst/frc/team4688/frc2018/Robot.java
+++ src/org/usfirst/frc/team4688/frc2018/Robot.java
@@ -2,7 +2,25 @@
 
 package org.usfirst.frc.team4688.frc2018;
 
+import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.*;
 
 public class Robot extends IterativeRobot
-{}
+{
+	private Dashboard dashboard;
+	
+	public void robotInit()
+	{
+		this.dashboard = new Dashboard();
+	}
+	
+	private class Dashboard
+	{
+		NetworkTable table;
+		
+		public Dashboard()
+		{
+			this.table = NetworkTableInstance.getDefault().getTable("SaintsBotDS");
+		}
+	}
+}
